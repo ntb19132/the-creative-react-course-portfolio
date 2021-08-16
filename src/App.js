c8d76a04f90c12d32bb03b87d76991fd469befbe
Nattapat Siripin <ntb19132@gmail.com>
2021-08-16 15:11:28 +0700

    Add Intro to styled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
+//Impage Page
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
