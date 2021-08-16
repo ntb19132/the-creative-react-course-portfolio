08e3127ebed6e81893d85a13aa77c7465cbaeffa
Nattapat Siripin <ntb19132@gmail.com>
2021-08-16 14:20:10 +0700

    add about section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Different payment method.</h4>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What product do you offer.</h4>
+          <div className="answer">
+            <p>Lorem ipsum, dolor sit amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+          </div>
+        </div>
+      </h2>
+    </div>
+  );
+};
+
+export default FaqSection;
